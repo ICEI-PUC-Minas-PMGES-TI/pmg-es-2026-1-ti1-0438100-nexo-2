465789a58349fcbfa7012015baa22a6afca5ed35
Rafael Felipe <rfelipe2007@gmail.com>
2026-05-25 11:05:40 -0300

    Implementando mudanças na estrutura da página com boostrap e implementando media queries para deixar a página responsiva.

--- codigo/public/modulos/prefeitura/editor_denuncias/editor.js
+++ codigo/public/modulos/prefeitura/editor_denuncias/editor.js
@@ -1,14 +1,15 @@
 const params = new URLSearchParams(window.location.search);
 const id = parseInt(params.get('id'));
 
-fetch('editor.json')
+fetch(`http://localhost:3000/denuncias/${id}`,{
+    headers: {
+        "Accept": "application/json"
+    }
+})
     .then(response => response.json())
-    .then(dados => {
-        const denuncia = dados.denuncias.find(d => d.id === id);
-        if (denuncia) {
-            preencherModal(denuncia);
-        }
-    });
+    .then(denuncia => {
+        preencherModal(denuncia);
+    })
 
 function preencherModal(denuncia){
     document.getElementById('nota').value = denuncia.notaOrgao;
@@ -23,3 +24,30 @@ function preencherModal(denuncia){
     document.getElementById('descricao-custo').value = denuncia.descricaoCusto;
     document.getElementById('prazo').value = denuncia.prazo;
 }
+
+document.querySelector('.btn-salvar').addEventListener('click', function(){
+    const dadosAtualizados = {
+        notaOrgao: document.getElementById('nota').value,
+        checkpoints: {
+            denunciaAceita: document.getElementById('checkpoint-1').checked,
+            equipeEnviada: document.getElementById('checkpoint-2').checked,
+            planejamento: document.getElementById('checkpoint-3').checked,
+            manutencaoAndamento: document.getElementById('checkpoint-4').checked,
+            obraFinalizada: document.getElementById('checkpoint-5').checked,
+        },
+        custo: document.getElementById('valor').value.replace('R$ ', ''),
+        descricaoCusto: document.getElementById('descricao-custo').value,
+        prazo: document.getElementById('prazo').value
+    };
+    fetch(`http://localhost:3000/denuncias/${id}`,{
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dadosAtualizados)
+    })
+    .then(response => response.json())
+    .then(() => {
+        alert('Denúncia atualizada com sucesso!');
+    });
+});
